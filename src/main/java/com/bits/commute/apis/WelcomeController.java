ecac9aa4f4a5d2cb34573925c947b9d1e8e258bf
Vaibhav <vaibhav123.pawar@gmail.com>
2018-10-13 12:04:53 +0530

    Initial Setup

---
+++ src/main/java/com/bits/commute/apis/WelcomeController.java
@@ -0,0 +1,20 @@
+package com.bits.commute.apis;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bits.commute.vo.GreetingResponseVO;
+
+@RestController
+public class WelcomeController {
+	@RequestMapping(value="/greetings", method=RequestMethod.GET)
+	public @ResponseBody GreetingResponseVO greetings() {
+		
+		GreetingResponseVO greeting = new GreetingResponseVO();
+		greeting.setGreeting("Welcome");
+		greeting.setName("Vaibhav");
+		return greeting;
+	}
+}
---
+++ src/main/java/com/bits/commute/vo/GreetingResponseVO.java
@@ -0,0 +1,22 @@
+package com.bits.commute.vo;
+
+public class GreetingResponseVO {
+	private String name;
+	private String greeting;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGreeting() {
+		return greeting;
+	}
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+	
+	
+}
